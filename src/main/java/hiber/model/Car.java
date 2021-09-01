9e2d44f7dc1059addda6b0b2e554cc79a72301cf
Mirseit <mibraimov250@gmail.com>
2021-09-02 00:47:39 +0600

    пытяюсь исправить ошибку...

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,69 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Column (name = "model")
+    private String model;
+
+    @Column (name = "series")
+    private int series;
+
+    @OneToOne(mappedBy = "car")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Car() {
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series= series;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return model + " " + series;
+    }
+}
